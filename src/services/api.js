86f63cee9f5a0db93b98ef4572e4d02c02133863
RobertoMancilla <mancillarober24@gmail.com>
2026-03-11 13:48:46 -0600

    corrigiendo front

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 
 export const productsAPI = axios.create({
-  baseURL: "/",
+  baseURL: import.meta.env.VITE_PRODUCTS_API_URL || "/",
   timeout: 10000,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 export const ordersAPI = axios.create({
-  baseURL: "/",
+  baseURL: import.meta.env.VITE_ORDERS_API_URL || "/",
   timeout: 10000,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
